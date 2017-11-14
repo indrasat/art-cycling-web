6ff897c3c477ead9b1b304a58c84984b97582121
indrasat <indra.kurniawan717@gmail.com>
2017-11-14 21:34:13 +0700

    Update

--- app-server.js
+++ app-server.js
@@ -10,13 +10,22 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 var http = require('http').Server(app)
 
+var xoauth2 = require("xoauth2"),
+xoauth2gen;
+
+xoauth2gen = xoauth2.createXOAuth2Generator({
+  user: "gaung717@gmail.com",
+  clientId: "21247606951-r4ak47st49f05ji53v0va9mu1p3a7fff.apps.googleusercontent.com",
+  clientSecret: "NezG50AyOM3Ljm7bv7CdA-uV",
+  refreshToken: "1/dCupAEK48O6p_Fh4zOXKXv5VC0Hqlo_X5Pzgyz32AOZnrIWeC_1GIIdt2N5DvfVN",
+  accessToken:"ya29.GlsEBU7JNoZiyXq1dYB33GwWF7sMKHaRDRr4DMSHJOl5-YbWzjqRZuPPRb3-EhzhrHvMVMZtAXJGU0y_151RZjL2JjSBk5vY91oO3Q8ycGcApSSLmUyV9O2hVHoe"
+});
   /*** Send Email  */
   var transporter = nodemailer.createTransport(smtpTransport({
     service: 'gmail',
     auth: {
-        user: 'gaung717@gmail.com', // my mail
-        pass: '80080pitu-o'
-    }
+      xoauth2: xoauth2gen,
+    },
   }));  
   let HelperOptions = {
     from: '"Indra" <gaung717@gmail.com>',
@@ -35,9 +44,11 @@ var http = require('http').Server(app)
       if (error) {
         console.log(error);
         res.json({yo: error});
+      }else{
+        console.log("The message was sent!");
+        console.log(info);
       }
-      console.log("The message was sent!");
-      console.log(info);
+
     });
   })
 
